566518b886a4efa81fe29883efa637e986a5bb28
rezan83 <rezan.moh83@gmail.com>
2020-08-04 11:12:24 +0200

    add root url

--- djcommerce/urls.py
+++ djcommerce/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('commerce.urls')),
     path('home/', include('commerce.urls')),
 ]
 
