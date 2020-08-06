2879879c672dd5ffeca6961ef60f279bbadf0f37
rezan83 <rezan.moh83@gmail.com>
2020-08-06 22:18:55 +0200

    sss

--- djcommerce/settings.py
+++ djcommerce/settings.py
@@ -37,7 +37,8 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG') == "True"
+DEBUG = True
+# DEBUG = os.getenv('DEBUG') == "True"
 
 ALLOWED_HOSTS = ['myestora.herokuapp.com', '127.0.0.1']
 
